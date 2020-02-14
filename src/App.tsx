eeb1a56bbd8d3841a9f68725bfe1bcc9e79f29ff
TuxBoy <benoit.mallo@gmail.com>
2020-02-14 19:05:36 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App container">
+      <h1>Tous les derniers liens</h1>
+    </div>
+  );
+}
+
+export default App;
